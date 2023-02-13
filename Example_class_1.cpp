be5ad3f012cabf7b04edf6d381444cbfdf13d074
keykey <keyyew@gmail.com>
2023-02-13 17:36:04 +0800

    initial commit

---
+++ Example_class_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+vector<int> hybridSort(vector<int> &arr)
+{
+    // todo
+}
+
+vector<int> mergeSort(vector<int> &arr)
+{
+    // todo
+}
+
+int main()
+{
+
+    // generate datasets
+
+    hybridSort();
+    mergeSort();
+
+    // plot number of key comparisons over different size n with fixed S
+
+    // plot number of key comparisons over different size n with different S
+    return 0;
+}
