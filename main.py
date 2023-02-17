6442464ab0333aca8d1ed3f8f11981847f8dd3ee
keykey <keyyew@gmail.com>
2023-02-17 14:56:16 +0800

    edited generateData

---
+++ main.py
@@ -0,0 +1,81 @@
+from numpy import random
+
+# Merge sort taught in lecture
+
+
+def mergeSort(arr):
+    n = 0
+    m = len(arr) - 1
+    midPoint = (n+m)//2
+    if (m-n <= 0):
+        return arr
+    else:
+        lArr = mergeSort(arr[:midPoint+1])
+        rArr = mergeSort(arr[midPoint+1:])
+
+    return merge(lArr, rArr)  # merge to handle 2 elements
+
+
+def generatedata(min, max, x):
+    inputData = []
+    size = min
+    while size <= max:
+        print("generating..")
+        arr = [random.randint(1, x) for _ in range(size)]
+        inputData.append(arr)
+        size *= 10
+    return inputData
+
+
+def insertionsort(arr):
+    global count
+    for i in range(1, len(arr)):
+        for j in range(i, 0, -1):
+            count += 1
+            if (arr[j] < arr[j-1]):
+                temp = arr[j]
+                arr[j] = arr[j-1]
+                arr[j-1] = temp
+    return arr
+
+
+def merge(lArr, rArr):
+    global count
+    i = 0
+    j = 0
+    res = []
+    while i <= len(lArr)-1 and j <= len(rArr)-1:
+
+        count += 1
+        if lArr[i] < rArr[j]:
+            res.append(lArr[i])
+            i += 1
+        else:
+            res.append(rArr[j])
+            j += 1
+    if (i != len(lArr)):
+        res += lArr[i:]
+    else:
+        res += rArr[j:]
+
+    return res
+
+
+def hybridsort(arr, S):
+    if (len(arr) <= S):
+        return insertionsort(arr)
+    midPoint = len(arr)//2
+    lArr = hybridsort(arr[:midPoint], S)
+    rArr = hybridsort(arr[midPoint:], S)
+    return merge(lArr, rArr)
+
+
+arrs = generatedata(1000, 10000000, 1000000)
+counter = {}
+count = 0
+for arr in arrs:
+    print("sorting..")
+    count = 0
+    hybridsort(arr, 50)
+    counter[len(arr)] = count
+print(counter)
--- merge_sort.py
+++ merge_sort.py
@@ -1,53 +1,37 @@
-#Merge sort taught in lecture
-def mergesort(n,m):
-    midPoint=(n+m)//2
-    if (m-n<=0):
-        return
-    elif (m-n>1):
-        mergesort(n,midPoint)
-        mergesort(midPoint+1,m)
-    merge(n,m) #merge to handle 2 elements
-    return
+# Merge sort taught in lecture
+def mergeSort(arr):
+    n = 0
+    m = len(arr) - 1
+    midPoint = (n+m)//2
+    if (m-n <= 0):
+        return arr
+    else:
+        # print("lArr:", arr[:midPoint+1])
+        lArr = mergeSort(arr[:midPoint+1])
+        # print("rArr:", arr[midPoint+1:])
+        rArr = mergeSort(arr[midPoint+1:])
 
-def merge(n,m):
-    #print(f'{arr[n]} to {arr[m]}')
-    if (m-n<=0):
-        return
-    midPoint=(n+m)//2
-    a=n #a and b are running indexes
-    b=midPoint+1
+    return merge(lArr, rArr)  # merge to handle 2 elements
 
-    while (a<=midPoint and b <=m): #two halves not empty
-        if (arr[a]<arr[b]):
-            a+=1
-            n+=1 #shift future place of where num will be inserted
-        elif (arr[a]>arr[b]):
-            temp=arr[b]
-            arr[n+1:b+1] = arr[n:b]
-            arr[n]=temp #plug back the temp variable
-            
-            b+=1
-            n+=1 #accommodate b
-            a+=1 #due to index shift 
-            midPoint+=1 #due to index shift 
-        else: #a==b
-            if (a==midPoint and b==m): #not sure whats the reason for this
-                break
-            a+=1  # accommodate a
-            n+=1
-            
-            temp=arr[b]  #accommodate b
-            # while (i != n):
-            #     arr[i] = arr[i-1]
-            #     i -= 1
-            arr[n+1:b+1] = arr[n:b]
-            arr[n]=temp
-            n+=1
-            b+=1
-            a+=1
-            midPoint+=1
-    return
-        
-arr=[1,2,3,1,2,5,3,4,1,2,3]
-mergesort(0,len(arr)-1)
-print(arr)
+
+def merge(lArr, rArr):
+    i = 0
+    j = 0
+    res = []
+    while i <= len(lArr)-1 and j <= len(rArr)-1:
+        if lArr[i] < rArr[j]:
+            res.append(lArr[i])
+            i += 1
+        else:
+            res.append(rArr[j])
+            j += 1
+    if (i != len(lArr)):
+        res += lArr[i:]
+    else:
+        res += rArr[j:]
+
+    return res
+
+
+arr = [1, 2, 3, 1, 2, 5, 3, 4, 1, 2, 3]
+print(mergeSort(arr))
